64bfdae7371ba5c3eb9f74f62c6abf84c53d6908
Your Name <you@example.com>
2022-06-12 11:03:32 -0400

    Add result filter pagination header

---
+++ BookingApp.Presentation/ResultFilters/PaginationHeaderFilterAttribute.cs
@@ -0,0 +1,30 @@
+﻿using BookingApp.DomainLayer.Options;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Text.Json;
+
+namespace BookingApp.Presentation.ResultFilters;
+public class PaginationHeaderFilterAttribute<TResult> : IAsyncResultFilter
+{
+    public async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+    {
+        var results = context.Result;
+
+#pragma warning disable CS8605 // Unboxing a possibly null value.
+        if (context.HttpContext.Response.StatusCode is 200)
+        {
+            OkObjectResult resultObjectArray = (OkObjectResult)results;
+            (IEnumerable<TResult> result, MetaData metaData) = ((IEnumerable<TResult>, MetaData))resultObjectArray.Value;
+            context.HttpContext.Response.Headers.Add("X-Pagination", JsonSerializer.Serialize(metaData));
+
+            resultObjectArray.Value = result;
+        }
+        await next();
+
+#pragma warning restore CS8605 // Unboxing a possibly null value.
+
+
+
+    }
+}
+
